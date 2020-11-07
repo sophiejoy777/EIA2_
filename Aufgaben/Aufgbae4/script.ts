0684d897c3f539b87b99bc383c6ab63b94b8df9b
sophiejoy777 <56411376+sophiejoy777@users.noreply.github.com>
2020-11-07 15:00:42 +0100

    Add files via upload

---
+++ Aufgaben/Aufgbae4/script.ts
@@ -0,0 +1,24 @@
+
+var L04_Cauldron;
+(function (L04_Cauldron) {
+    window.addEventListener("load", handleLoad);
+    let totalCost = 0;
+    let totalPrice = document.querySelector("#price");
+    let name = document.querySelector(#potion);
+    let description = document.querySelector(#description);
+    let effect = document.querySelector(#effect);
+    let effecttime = document.querySelector (#addeffect);
+
+
+    function handleLoad(_event) {
+        console.log("Start");
+        let form = document.querySelector("div#form");
+        form.addEventListener("change", handleChange);
+    }
+    function handleChange(_event) {
+        let dragon skin = document.querySelector("#dragon skin");
+        let knotgrass = document.querySelector("#knotgrass");
+        let hair = document.querySelector("#hair");
+        let recipe = document.querySelector("#display");
+        recipe.innerHTML = "";
+        let formData = new FormData(document.forms[0]);
